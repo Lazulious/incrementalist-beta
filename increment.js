56842130825cf45324e42fa70e5c187d107c7bbe
Lazulious <65876686+Lazulious@users.noreply.github.com>
2020-09-05 16:31:44 -0500

    Update increment.js

--- increment.js
+++ increment.js
@@ -1,30 +1,43 @@
-function increment(type, num, update) {
-  if (typeof update == "undefined") {update = true}
-  if (type == 'x' && clickrate < 15) {
-    clickrate++;
-    user.ip.x = user.ip.x.plus(getincxx());
-    user.ip.sac = user.ip.sac.plus(getincxx());
-    user.ip.pp = user.ip.pp.plus(getincxx());
-    user.ip.total = user.ip.total.plus(getincxx());
-    if (update) {updateip(); updatescaleinc(); updatesacip(); unlockip()}
-    return;
+//Buttons
+function increment() {
+  user.ip.x = user.ip.x.plus(getIncrementx());
+  user.ip.sac = user.ip.sac.plus(getIncrementx());
+}
+function buyIncrement(v, n) {
+  let cost = getIncrement(v, n, "Cost");
+  if (user.ip.x.gte(cost)) {
+    user.ip.x = user.ip.x.minus(cost);
+    user.increment[v][n]++;
+    unlockIP();
   }
-  if (type == 'p' && user.ip.x.gte(getincp(num, "cost"))) {
-    user.ip.x = user.ip.x.minus(getincp(num, "cost"));
-    user.inc.p[num]++;
-    if (update) {updateip(); updateincx(); updateincp()}
-    return;
+}
+
+//Get Data
+function getIncrementx() {
+  let pn = -1;
+  for (let i = 0; i < 5; i++) {if (d("incrementP" + i).style.display != "none") {pn = i}}
+  let p = getIncrement("p", pn, "x");
+  for (let i = (pn - 1); i > -1; i--) {p = p.plus(getIncrement("p", i, "x"))}
+  return p;
+}
+function getIncrement(v, n, type) {
+  if (v == "p") {
+    if (type == "x") {return nd(n + 1).pow(nd(n)).times(user.increment.p[n])}
+    else if (type == "Cost") {return nd(1).plus(nd(0.125).times(nd(2).pow(n))).pow(user.increment.p[n]).floor()}
   }
-  if (type == 'm' && user.ip.x.gte(getincm(num, "cost"))) {
-    user.ip.x = user.ip.x.minus(getincm(num, "cost"));
-    user.inc.m[num]++;
-    if (update) {updateip(); updateincx(); updateincm()}
-    return;
-  }
-  if (type == 'e' && user.ip.x.gte(getince(num, "cost"))) {
-    user.ip.x = user.ip.x.minus(getince(num, "cost"));
-    user.inc.e[num]++;
-    if (update) {updateip(); updateincx(); updateince()}
-    return;
+}
+
+//Update Data
+function updateEquationIP() {
+  let xn = -1;
+  for (let i = 0; i < 5; i++) {if (d("incrementP" + i).style.display != "none") {xn = i}}
+  let x = getIncrement("p", xn, "x");
+  for (let i = (xn - 1); i > -1; i--) {x = x.plus(getIncrement("p", i, "x"))}
+  d("equationP").textContent = x;
+}
+function updateIncrement(v, n, type) {
+  let id = "increment" + v.toUpperCase() + n + type;
+  if (d(id).style.display != "none") {
+    d(id).textContent = e(getIncrement(v, n, type));
   }
 }
